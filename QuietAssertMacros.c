83b2a0da48b9bbed15a5d081e1f7cf95a0990ef8
Maxwin-z <zwd2005@gmail.com>
2013-09-10 23:27:31 +0800

    newsmth

---
+++ QuietAssertMacros.c
@@ -0,0 +1,35 @@
+//
+//  QuietAssertMacros.c
+//  newsmth
+//
+//  Created by damu on 9/10/13.
+//  Copyright (c) 2013 nju. All rights reserved.
+//
+
+#include <stdio.h>
+#include <dlfcn.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include "fishhook.h"
+
+int slient_fprintf(FILE * restrict stream, const char * restrict format, ...)
+{
+    if (strncmp(format, "AssertMacros:", 13) == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        va_list args;
+        va_start(args, format);
+        int result = vfprintf(stream, format, args);
+        va_end(args);
+        return result;
+    }
+}
+
+__attribute__((constructor)) void QuietAssertMacros(void)
+{
+    rebind_symbols((struct rebinding[1]){{"fprintf", slient_fprintf}}, 1);
+}
